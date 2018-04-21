333a881690679171ede4bf043bbc89af96964f47
franciscooliver <frandosax@gmail.com>
2018-04-21 13:37:08 -0300

    inserindo_oficinas

---
+++ app/Http/Controllers/OficinaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Oficina;
+
+class OficinaController extends Controller
+{
+
+  public function  __construct(){
+      header("Acess-Control-Allow-Origin:*");
+  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $dados = $request->all();
+      $oficina = Oficina::create($dados);
+
+      if($oficina){
+        return response()->json(['data'=>$oficina,'status'=>true]);
+      }else{
+        return response()->json(['data'=>"Erro ao salvar produto",'status'=>false]);
+      }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $oficina = Oficina::find($id);
+
+        return response()->json(['data'=>$oficina,'status'=>true]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
